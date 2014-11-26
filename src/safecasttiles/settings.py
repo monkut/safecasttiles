07c2710412e3ca7f488d63890ec06d4dd6ff8899
q16127 <shane.cousins@nokia.com>
2014-11-26 15:06:59 +0900

    adjusting BASE_DIR & STATIC_ROOT.

--- src/safecasttiles/settings.py
+++ src/safecasttiles/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(__file__)
 
 
 # Quick-start development settings - unsuitable for production
@@ -75,4 +75,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/'
-STATIC_ROOT = os.path.join(BASE_DIR, os.path.basename(__file__), "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
