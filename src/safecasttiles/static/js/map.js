8fd8bdee07012e0ec54383d47025a6500898a782
q16127 <shane.cousins@nokia.com>
2014-11-26 15:47:26 +0900

    setting localTileLayerUrl to to static ip for testing.

--- src/safecasttiles/static/js/map.js
+++ src/safecasttiles/static/js/map.js
@@ -11,7 +11,7 @@ function initmap() {
 	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
 	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});
-    var localTileLayerUrl = 'http://127.0.0.1:8000/tiles/201108/{z}/{x}/{y}.png';
+    var localTileLayerUrl = 'http://10.143.166.17:8000/tiles/201108/{z}/{x}/{y}.png';
 	var localTileLayer = new L.TileLayer(localTileLayerUrl, {minZoom: 8, maxZoom: 12, attribution: "safecast.org"});
 
 	// start the map in Fukushima
