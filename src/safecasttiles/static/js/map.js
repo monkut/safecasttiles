5b626757441f854abb3734ecf6dd5a6ab38caa84
q16127 <shane.cousins@nokia.com>
2014-12-01 13:58:27 +0900

    commenting out osm layer for now, need to get map data through proxy.

--- src/safecasttiles/static/js/map.js
+++ src/safecasttiles/static/js/map.js
@@ -15,9 +15,9 @@ function initmap() {
 	map = new L.Map('map');
 
 	// create the tile layer with correct attribution
-	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});
+	//var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	//var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+	//var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});
 	var localLayerName = getParameterByName("layer");
 	if (!localLayerName){
 	    localLayerName = "201112";
@@ -42,6 +42,6 @@ function initmap() {
 
 	// start the map in Fukushima
 	map.setView(new L.LatLng(37.435793, 140.735437), 9);
-	map.addLayer(osm);
+	//map.addLayer(osm);
 	map.addLayer(localTileLayer);
 }
