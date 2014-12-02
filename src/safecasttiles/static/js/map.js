6b51f002600dd3a14346d0c189e4a3f16c955f39
q16127 <shane.cousins@nokia.com>
2014-12-02 18:20:54 +0900

    clarifying console debug

--- src/safecasttiles/static/js/map.js
+++ src/safecasttiles/static/js/map.js
@@ -43,10 +43,9 @@ function initmap() {
         // parse localLayerName to int year and month to create display timeline overlay
         var selectedLayerYear = parseInt(localLayerName.substring(0, 4));
         var selectedLayerMonth = parseInt(localLayerName.substring(4, 6));
-        console.log(localLayerName);
-        console.log(selectedLayerYear);
-        console.log(selectedLayerMonth);
-        //this._div.innerHTML = '<h4>' + localLayerName + '</h4>';
+        console.log("localLayerName: " + localLayerName);
+        console.log("Parsed selectedLayerYear: " + selectedLayerYear);
+        console.log("Parsed selectedLayerMonth: " + selectedLayerMonth);
         this._div.innerHTML = getTimeline(selectedLayerYear, selectedLayerMonth);
     };
     info.addTo(map);
