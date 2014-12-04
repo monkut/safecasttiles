5946b810a02f4f31c42f5e4f141d0f8fdbfbc710
q16127 <shane.cousins@nokia.com>
2014-12-04 14:06:45 +0900

    temporarly serving local maps

--- src/safecasttiles/static/js/map.js
+++ src/safecasttiles/static/js/map.js
@@ -48,6 +48,11 @@ function initmap() {
 	//var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 	//var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
 	//var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});
+	var osmUrl='http://10.143.237.194/{z}/{x}/{y}.png';
+	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});
+
+
 
     L.control.scale({metric: true, imperial: false}).addTo(map);
 
@@ -90,6 +95,6 @@ function initmap() {
 
 	// start the map in Fukushima
 	map.setView(new L.LatLng(37.435793, 140.735437), 9);
-	//map.addLayer(osm);
+	map.addLayer(osm);
 	map.addLayer(localTileLayer);
 }
